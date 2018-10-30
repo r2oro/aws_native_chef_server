2c642b7674f326e8e216288b69f8ee63bc6520da
212481589 <artur.pioro@ge.com>
2018-10-30 13:28:17 +0100

    Setup GESOS AMIs

--- generate_os_mappings.sh
+++ generate_os_mappings.sh
@@ -1,23 +1,31 @@
 #!/bin/bash
 
-# get the latest Name value from: 
+# get the latest Name value from:
 # RHEL: aws ec2 describe-images --owners 309956199498 --filters "Name=name,Values=RHEL-7.6*" --query "Images[*].Name" --output text
 # CentOS highperf: `aws ec2 describe-images --owners 446539779517 --filters "Name=name,Values=chef-highperf-centos7*" --query "Images[*].Name" | sort`
 
-RHEL_RELEASE='RHEL-7.6_HVM_BETA-20180814-x86_64-0-Hourly2-GP2'
-CENTOS_RELEASE='chef-highperf-centos7-201808171554'
+# RHEL_RELEASE='RHEL-7.6_HVM_BETA-20180814-x86_64-0-Hourly2-GP2'
+# CENTOS_RELEASE='chef-highperf-centos7-201808171554'
+
+# Query GESOS Enterprise Cloud Build AMIs
+RHEL_RELEASE='GESOS-Cloud-RHEL7'
+CENTOS_RELEASE='GESOS-Cloud-CentOS7'
 
 printf "Mappings:\n  AMI:\n"
 
 regions=$(aws ec2 describe-regions --query "Regions[].RegionName" --output text)
 for region in $regions; do
   rhel_ami=$(aws --region $region ec2 describe-images \
-  --filters "Name=name,Values=${RHEL_RELEASE}" \
-  --query "Images[0].ImageId" --output "text")
+  --owners 277688789493 \
+  --filters "Name=name,Values=${RHEL_RELEASE}*" \
+  --query "Images[*].[CreationDate,Name,ImageId]" --output "text" | sort -r | head -1 | awk '{print $3}')
 
   centos_ami=$(aws --region $region ec2 describe-images \
-  --filters "Name=name,Values=${CENTOS_RELEASE}" \
-  --query "Images[0].ImageId" --output "text")
+  --owners 277688789493 \
+  --filters "Name=name,Values=${CENTOS_RELEASE}*" \
+  --query "Images[*].[CreationDate,Name,ImageId]" --output "text" | sort -r | head -1 | awk '{print $3}')
 
-  printf "    $region:\n      rhel: $rhel_ami\n      centos: $centos_ami\n"
+  [ -n "$rhel_ami" -o -n "$centos_ami" ] && printf "    $region:\n"
+  [ -n "$rhel_ami" ] && printf "      rhel: $rhel_ami\n"
+  [ -n "$centos_ami" ] && printf "      centos: $centos_ami\n"
 done
